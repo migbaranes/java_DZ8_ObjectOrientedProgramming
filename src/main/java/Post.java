8138207868b9d7a9f96eb3a4e289bca6474950e7
migbaranes <migbaranes@gmail.com>
2023-12-11 21:20:27 +0300

    First commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    // добавьте другие поля
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+}
